ec97bc1d2e24f7895118c7cea791dffcb09f7c2c
Ahmet Sait <ahmetsaitkocak@gmail.com>
2018-09-14 07:14:18 +0300

    Initial commit

---
+++ LF2 Sprite Sheet Generator/Form_Rename.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace LF2.Sprite_Sheet_Generator
+{
+	partial class Form_Rename
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button_Ok = new System.Windows.Forms.Button();
+			this.button_Cancel = new System.Windows.Forms.Button();
+			this.panel = new System.Windows.Forms.Panel();
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.label_Name = new System.Windows.Forms.Label();
+			this.panel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button_Ok
+			// 
+			this.button_Ok.Location = new System.Drawing.Point(3, 4);
+			this.button_Ok.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.button_Ok.Name = "button_Ok";
+			this.button_Ok.Size = new System.Drawing.Size(80, 30);
+			this.button_Ok.TabIndex = 2;
+			this.button_Ok.Text = "OK";
+			this.button_Ok.UseVisualStyleBackColor = true;
+			this.button_Ok.Click += new System.EventHandler(this.button_Ok_Click);
+			// 
+			// button_Cancel
+			// 
+			this.button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.button_Cancel.Location = new System.Drawing.Point(89, 4);
+			this.button_Cancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.button_Cancel.Name = "button_Cancel";
+			this.button_Cancel.Size = new System.Drawing.Size(80, 30);
+			this.button_Cancel.TabIndex = 3;
+			this.button_Cancel.Text = "Cancel";
+			this.button_Cancel.UseVisualStyleBackColor = true;
+			this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+			// 
+			// panel
+			// 
+			this.panel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.panel.AutoSize = true;
+			this.panel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.panel.Controls.Add(this.button_Ok);
+			this.panel.Controls.Add(this.button_Cancel);
+			this.panel.Location = new System.Drawing.Point(55, 70);
+			this.panel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.panel.Name = "panel";
+			this.panel.Size = new System.Drawing.Size(172, 38);
+			this.panel.TabIndex = 1;
+			// 
+			// textBox
+			// 
+			this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox.Location = new System.Drawing.Point(12, 33);
+			this.textBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.textBox.Name = "textBox";
+			this.textBox.Size = new System.Drawing.Size(258, 27);
+			this.textBox.TabIndex = 1;
+			// 
+			// label_Name
+			// 
+			this.label_Name.AutoSize = true;
+			this.label_Name.Location = new System.Drawing.Point(12, 9);
+			this.label_Name.Name = "label_Name";
+			this.label_Name.Size = new System.Drawing.Size(49, 20);
+			this.label_Name.TabIndex = 2;
+			this.label_Name.Text = "Name";
+			// 
+			// Form_Rename
+			// 
+			this.AcceptButton = this.button_Ok;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.button_Cancel;
+			this.ClientSize = new System.Drawing.Size(282, 121);
+			this.Controls.Add(this.label_Name);
+			this.Controls.Add(this.textBox);
+			this.Controls.Add(this.panel);
+			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form_Rename";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Rename";
+			this.panel.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button_Ok;
+		private System.Windows.Forms.Button button_Cancel;
+		private System.Windows.Forms.Panel panel;
+		public System.Windows.Forms.TextBox textBox;
+		private System.Windows.Forms.Label label_Name;
+	}
+}
---
+++ LF2 Sprite Sheet Generator/MainForm.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LF2.Sprite_Sheet_Generator
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			splitContainer.SplitterDistance = 800;
+			Application.ThreadException += Application_ThreadException;
+		}
+
+		private void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+		{
+			ShowException(e.Exception);
+		}
+
+		float[] opacityList = { 0f, 0.10f, 0.25f, 0.50f, 0.75f, 0.90f, 1.00f };
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			toolStripComboBox_Opacity.SelectedIndex = 3;
+			templateBox.ScaleFit();
+			templateBox_OffsetChanged(templateBox, e);
+		}
+		
+		public void ShowException(Exception ex, string title = null, IWin32Window owner = null)
+		{
+			MessageBox.Show(owner ?? this, ex.ToString(), title ?? "Unhandled Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			AboutForm about = new AboutForm();
+			about.ShowDialog(this);
+		}
+
+		private void toolStripButton_Transparency_CheckedChanged(object sender, EventArgs e)
+		{
+			templateBox.GuideTransparency = toolStripButton_Transparency.Checked;
+		}
+
+		private void toolStripMenuItem_ChessBoard_Click(object sender, EventArgs e)
+		{
+			templateBox.BackgroundImage = Properties.Resources.check;
+			toolStripMenuItem_ChessBoard.Checked = true;
+			toolStripMenuItem_Color.Checked = false;
+		}
+
+		private void toolStripMenuItem_Color_Click(object sender, EventArgs e)
+		{
+			templateBox.BackgroundImage = null;
+			toolStripMenuItem_Color.Checked = true;
+			toolStripMenuItem_ChessBoard.Checked = false;
+		}
+
+		private void toolStripMenuItem_AdjustColor_Click(object sender, EventArgs e)
+		{
+			if (colorDialog.ShowDialog(this) == DialogResult.OK)
+			{
+				templateBox.BackColor = colorDialog.Color;
+				templateBox.BackgroundImage = null;
+				toolStripMenuItem_Color.Checked = true;
+				toolStripMenuItem_ChessBoard.Checked = false;
+				using (Graphics g = Graphics.FromImage(toolStripMenuItem_Color.Image))
+					g.Clear(templateBox.BackColor);
+				toolStripMenuItem_Color.Invalidate();
+			}
+		}
+		
+		private void toolStripButton_Move_Click(object sender, EventArgs e)
+		{
+			templateBox.EditMode = EditMode.Move;
+			toolStripButton_Move.Checked = true;
+			toolStripButton_Rotate.Checked = false;
+			toolStripButton_Sclale.Checked = false;
+		}
+
+		private void toolStripButton_Rotate_Click(object sender, EventArgs e)
+		{
+			templateBox.EditMode = EditMode.Rotate;
+			toolStripButton_Move.Checked = false;
+			toolStripButton_Rotate.Checked = true;
+			toolStripButton_Sclale.Checked = false;
+		}
+
+		private void toolStripButton_Sclale_Click(object sender, EventArgs e)
+		{
+			templateBox.EditMode = EditMode.Scale;
+			toolStripButton_Move.Checked = false;
+			toolStripButton_Rotate.Checked = false;
+			toolStripButton_Sclale.Checked = true;
+		}
+
+		private void toolStripButton_ScaleFit_Click(object sender, EventArgs e)
+		{
+			templateBox.ScaleFit();
+		}
+
+		private void toolStripButton_Scale1_Click(object sender, EventArgs e)
+		{
+			templateBox.Zoom = 1;
+		}
+
+		private void toolStripButton_Scale2_Click(object sender, EventArgs e)
+		{
+			templateBox.Zoom = 2;
+		}
+
+		private void toolStripSplitButton_GuideImage_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog_Image.ShowDialog(this) == DialogResult.OK && File.Exists(openFileDialog_Image.FileName))
+			{
+				templateBox.GuideImage = Image.FromFile(openFileDialog_Image.FileName);
+				templateBox.ScaleFit();
+			}
+		}
+
+		private void toolStripComboBox_Opacity_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (toolStripComboBox_Opacity.SelectedIndex >= 0)
+			{
+				templateBox.GuideImageAlpha = opacityList[toolStripComboBox_Opacity.SelectedIndex];
+			}
+		}
+
+		private void templateBox_ZoomChanged(object sender, EventArgs e)
+		{
+			toolStripStatusLabel_Zoom.Text = templateBox.Zoom.ToString("F2");
+		}
+
+		private void templateBox_OffsetChanged(object sender, EventArgs e)
+		{
+			toolStripStatusLabel_Offset.Text = templateBox.Offset.X.ToString("F2") + ", " + templateBox.Offset.Y.ToString("F2");
+			statusStrip.Refresh();
+		}
+
+		private void button_AddSprite_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog_Image.ShowDialog(this) == DialogResult.OK)
+			{
+				try
+				{
+					foreach (string file in openFileDialog_Image.FileNames)
+					{
+						string name = Path.GetFileNameWithoutExtension(file);
+						Image img = Image.FromFile(file);
+
+						for (Form_Rename fr = new Form_Rename(name); templateBox.Sprites.ContainsKey(name); )
+						{
+							if (fr.ShowDialog(this) == DialogResult.OK)
+								name = fr.textBox.Text.Trim();
+						}
+						listBox_SpriteParts.Items.Add(name);
+						templateBox.Sprites.Add(name, img);
+					}
+				}
+				catch (Exception ex)
+				{
+					ShowException(ex, "Error Importing Sprites", this);
+				}
+			}
+		}
+		
+		private void button_RemoveSprite_Click(object sender, EventArgs e)
+		{
+			if (listBox_SpriteParts.SelectedIndex >= 0)
+			{
+				templateBox.Sprites.Remove(listBox_SpriteParts.SelectedItem as string);
+				listBox_SpriteParts.Items.RemoveAt(listBox_SpriteParts.SelectedIndex);
+			}
+		}
+
+		private void button_MoveUp_Click(object sender, EventArgs e)
+		{
+			if (listBox_SpriteParts.SelectedIndex >= 0)
+			{
+				int index = listBox_SpriteParts.SelectedIndex, newIndex = (index - 1).Clamp(0, listBox_SpriteParts.Items.Count - 1);
+				object item = listBox_SpriteParts.SelectedItem;
+				if (newIndex != index)
+				{
+					var temp = listBox_SpriteParts.Items[newIndex];
+					listBox_SpriteParts.Items[newIndex] = listBox_SpriteParts.Items[index];
+					listBox_SpriteParts.Items[index] = temp;
+					listBox_SpriteParts.SelectedIndex = newIndex;
+					listBox_SpriteParts.Refresh();
+				}
+			}
+		}
+
+		private void button_MoveDown_Click(object sender, EventArgs e)
+		{
+			if (listBox_SpriteParts.SelectedIndex >= 0)
+			{
+				int index = listBox_SpriteParts.SelectedIndex, newIndex = (index + 1).Clamp(0, listBox_SpriteParts.Items.Count - 1);
+				object item = listBox_SpriteParts.SelectedItem;
+				if (newIndex != index)
+				{
+					var temp = listBox_SpriteParts.Items[newIndex];
+					listBox_SpriteParts.Items[newIndex] = listBox_SpriteParts.Items[index];
+					listBox_SpriteParts.Items[index] = temp;
+					listBox_SpriteParts.SelectedIndex = newIndex;
+					listBox_SpriteParts.Refresh();
+				}
+			}
+		}
+
+		private void listBox_SpriteParts_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (listBox_SpriteParts.SelectedIndex >= 0)
+			{
+				drawBox_SpritePart.Image = templateBox.Sprites[listBox_SpriteParts.SelectedItem as string];
+				drawBox_SpritePart.Refresh();
+			}
+		}
+
+		private void checkBox_SpritePartsTransparency_CheckedChanged(object sender, EventArgs e)
+		{
+			templateBox.Transparency = drawBox_SpritePart.Trancparency = checkBox_SpritePartsTransparency.Checked;
+		}
+
+		private void button_Rename_Click(object sender, EventArgs e)
+		{
+			if (listBox_SpriteParts.SelectedIndex >= 0)
+			{
+				string name = (string)listBox_SpriteParts.SelectedItem, newName;
+				Form_Rename fr = new Form_Rename(name);
+				do
+				{
+					if (fr.ShowDialog(this) == DialogResult.OK)
+						newName = fr.textBox.Text.Trim();
+					else
+						return;
+				}
+				while (templateBox.Sprites.ContainsKey(newName));
+				templateBox.Sprites[newName] = templateBox.Sprites[name];
+				templateBox.Sprites.Remove(name);
+			}
+		}
+
+		private void listBox_SpriteParts_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			if (listBox_SpriteParts.SelectedIndex >= 0)
+			{
+				templateBox.Renders.Add(new Render(
+					(string)listBox_SpriteParts.SelectedItem,
+					new Transform() { Position = templateBox.Offset, Scale = 1, Rotation = 0 }
+				));
+			}
+		}
+	}
+}
